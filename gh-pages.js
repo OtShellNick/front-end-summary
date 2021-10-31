aed9c882474b92525b23031b7d1acec32f0e36e0
Aleksandr <a.gura@sobix.net>
2021-10-31 10:42:19 +0300

    up

--- gh-pages.js
+++ gh-pages.js
@@ -1,8 +1,8 @@
 const ghpages = require('gh-pages');
 const path = require('path');
 
-const BUILD = path.resolve(__dirname + '/web/build');
+const BUILD = path.resolve(__dirname + '/build');
 ghpages.publish(BUILD, {
     branch: 'gh-pages',
-    repo: 'https://github.com/OtShellNick/front-end-summary.git'
+    repo: 'https://git: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@github.com/OtShellNick/front-end-summary.git'
 }, (e) => console.log(e));
